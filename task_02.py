d41128b9c11e31c8a9791b39fad4ad5218bb0e00
Lalani Werake <lalanikw@gmail.com>
2015-03-22 21:04:00 +0000

    task_01.py

---
+++ task_02.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Provides player pairs for the game using enumerate."""
+
+import authentication
+import getpass
+
+
+def login(username, maxattempts):
+    """
+
+    """
+    while 
+    
